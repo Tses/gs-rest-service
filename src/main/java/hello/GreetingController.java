451631c09c4f847654ca4fda6f10d27ad6a39171
Yiannis Tsesmelis <tsesmelisi@unisystems.gr>
2018-07-10 17:41:26 +0300

    test

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello tses with nexus, %s!";
+    private static final String template = "Hello John with binary, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
